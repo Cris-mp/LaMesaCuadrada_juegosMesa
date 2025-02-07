7ec13e27e6881efc891e51bf54a549934591784f
Cris-mp <cristina.mgzp@gmail.com>
2025-02-07 12:45:08 +0100

    Creacion de las migraciones de las tablas editorial, tipos y juegos

---
+++ database/migrations/2025_02_07_111552_create_juegos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('juegos', function (Blueprint $table) {
+            $table->engine='InnoDB';
+            $table->id();
+            $table->string('nombre');
+            $table->double('precio');
+            $table->int('unidades');
+            $table->int('duracion');
+            $table->int('edad_min');
+            $table->int('jugadores_max');
+            $table->unsignedBigInteger('editorial_id');
+            $table->unsignedBigInteger('tipo_id');
+            $table->foreign('editorial_id')->references('id')->on('editorial')->onDelete('cascade');
+            $table->foreign('tipo_id')->references('id')->on('tipo')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('juegos');
+    }
+};
